49bbc7f5d79320d94dca44185f26b37a9dd9d9a2
jynoes <nsabido29@gmail.com>
2024-03-24 22:35:22 +0800

    update gsapfrom

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -3,16 +3,16 @@ import gsap from "gsap";
 
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
-  useGSAP(() => {
-    gsap.from("#green-box", {
-      x: 250,
-      repeat: -1,
-      yoyo: true,
-      rotation: 360,
-      duration: 2,
-      ease: "elastic"
-    })
-  }, [])
+  // useGSAP(() => {
+  //   gsap.from("#green-box", {
+  //     x: 250,
+  //     repeat: -1,
+  //     yoyo: true,
+  //     rotation: 360,
+  //     duration: 2,
+  //     ease: "elastic"
+  //   })
+  // }, [])
 
   return (
     <main>
